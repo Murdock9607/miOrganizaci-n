e0115d14670e370625625bc1d74647cacc3312a8
ANGEL FARID ARTURO GOMEZ <angelarturo@unicauca.edu.co>
2023-04-14 08:24:13 -0500

    Datos de footer corregidos

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -2,18 +2,18 @@ import "./Footer.css"
 const Footer = () =>{
     return <footer className="footer" style={{backgroundImage:"url(/img/Footer.png)"}}>
     <div className="redes">
-    <a href='https://www.aluracursos.com/'> 
+    <a href='https://www.facebook.com/farid.arturo.1/'> 
         <img src="/img/facebook.png" alt="Facebook"/>
     </a>
-    <a href='https://www.aluracursos.com/'> 
+    <a href='https://twitter.com/farid_arturo'> 
         <img src="/img/twitter.png" alt="twitter" />
     </a>
-    <a href='https://www.aluracursos.com/'> 
+    <a href='https://www.instagram.com/farid_arturo/'> 
         <img src="/img/instagram.png" alt="instagram" />
     </a>
     </div>
     <img src='/img/Logo.png' alt='org' /> 
-    <strong>Desarrollado por Alura</strong>
+    <strong>Desarrollado por Angel Arturo</strong>
 </footer>
 }
 
